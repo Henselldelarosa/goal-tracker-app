ce9442f4c5bca615bf90304f348cd9d548f0195a
henselldelarosa <hensell1234@gmail.com>
2024-06-20 22:34:17 -0400

    input area done

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
         <Button title='Add Goal'/>
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -21,20 +21,32 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+
   appContainer: {
-    padding: 60
+    flex:1,
+    paddingTop: 60,
+    paddingHorizontal:16
   },
 
   inputContainer: {
+    flex:1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: 'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc'
   },
 
   textInput: {
     borderWidth: 1,
     borderBlockColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8
+  },
+
+  goalsContainer:{
+    flex: 5
   }
 });
